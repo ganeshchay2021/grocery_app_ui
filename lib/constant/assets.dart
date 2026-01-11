12b4d37ca64eaa3864a5f25fd5fe7b6bef598f20
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-01-11 19:35:49 +0545

    Feat(Assets,Constant): Assets path and App's constant color added

---
+++ lib/constant/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  static const String apple = "assets/images/apple.png";
+  static const String broccoli = "assets/images/broccoli.png";
+  static const String carrot = "assets/images/carrot.png";
+  static const String celery = "assets/images/celery.png";
+  static const String grape = "assets/images/grape.png";
+  static const String kfcChicken = "assets/images/kfc chicken.png";
+  static const String lettuce = "assets/images/lettuce.png";
+  static const String mango = "assets/images/mango.png";
+  static const String melon = "assets/images/melon.png";
+  static const String onboardingProfile =
+      "assets/images/onboarding_profile.png";
+  static const String orange = "assets/images/orange.png";
+  static const String paneer = "assets/images/paneer.png";
+  static const String potato = "assets/images/potato.png";
+}
